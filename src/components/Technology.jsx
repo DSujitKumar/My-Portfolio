585350847832bb2442fff00a4696df2bcdf2d689
Sujit Kumar Das <35437232+DSujitKumar@users.noreply.github.com>
2024-08-08 19:07:45 +0530

    Update Technology.jsx

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -26,16 +26,14 @@ const Technology = () => {
   return (
     <div className="border-b border-neutral-900 text-4xl">
       <motion.h1
-      whileInView={{opacity:1,y:0}}
-      initial={{opacity:0,y:-100}}
-      transition={{duration:1.5}}
+     
        className="my-20 text-center text-4xl">
         Technologies
       </motion.h1>
       <motion.div 
        whileInView={{opacity:1,z:0}}
-       initial={{opacity:0,z:-100}}
-       transition={{duration:1.5}}
+       initial={{opacity:0,z:-200}}
+       transition={{duration:2.5}}
        className="flex flex-wrap items-center justify-center gap-4 mb-20">
         <motion.div 
         
