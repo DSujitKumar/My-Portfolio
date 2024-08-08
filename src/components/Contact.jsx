5e969d13af33945fdc6d894fc55b122d5d385bce
Sujit Kumar Das <35437232+DSujitKumar@users.noreply.github.com>
2024-08-08 19:48:07 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,16 +5,14 @@ import {motion} from "framer-motion"
 const Contact = () => {
   return (  
     <div className="border-b border-neutral-900 text-4xl">
-      <motion.h1 whileInView={{opacity:1,y:0}}
-      initial={{opacity:0,y:-100}}
-      transition={{duration:1.5}}
+      <motion.h1 
       className="my-10 text-center text-4xl">
         Get in Touch 
       </motion.h1>       
         <motion.div 
        whileInView={{opacity:1,x:0}}
-       initial={{opacity:0,x:-100}}
-       transition={{duration:1.5}}
+       initial={{opacity:0,x:-50}}
+       transition={{duration:0.5}}
         className="text-center tracking-tighter text-sm">
             <p className="">{CONTACT.address}</p>
             <p className="">{CONTACT.phoneNo}</p>
