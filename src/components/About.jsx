a8a85a4577374971f06358b99d09a7a7674f86d8
Sujit Kumar Das <35437232+DSujitKumar@users.noreply.github.com>
2024-08-08 19:01:52 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,7 @@ const About = () => {
         </h1>
         <div className="flex flex-wrap">
             <motion.div
-            whileInView={{opacity:1,x:0}}
-            initial={{opacity:0,x:-100}}
-            transition={{duration:0.5}} className="w-full lg:w-1/2 lg:p-8">
+             className="w-full lg:w-1/2 lg:p-8">
                 <div className="flex items-center justify-center">
                     <img className="rounded-2xl" src={aboutImg} alt="About Me" />
                 </div>
